bf22445382d65ba61f6eef7b3486b5598c700f51
Luke Tuttle <39096324+LukeTuttle@users.noreply.github.com>
2021-09-21 16:39:26 -0600

    new features to rock paper scissors

--- RB101/lesson_2/rock_paper_scissors.rb
+++ RB101/lesson_2/rock_paper_scissors.rb
@@ -1,25 +1,42 @@
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = %w(rock paper scissors lizard spock)
+SCORE_NEEDED_FOR_WIN = 5
+WHO_BEATS_WHO = {
+  'rock': %w(lizard scissors),
+  'paper': %w(rock spock),
+  'scissors': %w(paper lizard),
+  'lizard': %w(spock paper),
+  'spock': %w(rock scissors)
+}
 
 def prompt(message)
   puts("=> #{message}")
 end
 
-def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
-end
-
-def display_results(player, computer)
-  if win?(player, computer)
-    prompt("You won!")
-  elsif win?(computer, player)
-    prompt("The computer won!")
-  else
-    prompt("It's a tie!")
+def display_results(result)
+  case result
+    when 1
+       prompt("YOU won!")
+    when 2
+       prompt("The COMPUTER won!")
+    when 3
+       prompt("It's a tie!") 
   end
 end
 
+def win?(first, second)
+  if first == second
+    3 # tie
+  elsif WHO_BEATS_WHO[first.to_sym].include?(second)
+    1 # first wins
+  else
+    2 # second wins
+  end
+end
+
+# Program start
+
+score_count = [0, 0]
+trial_count = 0
 loop do
   choice = ''
   loop do
@@ -34,13 +51,32 @@ loop do
   end
 
   computer_choice = VALID_CHOICES.sample
-
+  
+  trial_count += 1
+  prompt("TRIAL #: #{trial_count}")
   prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
 
-  display_results(choice, computer_choice)
+  #require('pry')
+  #binding.pry
+  who_won = win?(choice, computer_choice)
+  score_count[who_won - 1] += 1 unless who_won == 3
+  puts("------")
+  display_results(who_won)
+  break if score_count.max >= SCORE_NEEDED_FOR_WIN
 
-  prompt("Do you want to play agian?")
+  prompt("Score after #{trial_count} trials")
+  puts("     player: #{score_count[0]}")
+  puts("     computer: #{score_count[1]}")
+  
+  
+  prompt("Do you want to play agian? (y/n):")
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
-  prompt("Thank you for playing. Good bye!")
 end
+
+you_won = "Congratulations! You won the match!"
+you_lost = "Whaap Whaap...You lost the match"
+score_count[0] > score_count[1] ? prompt(you_won): prompt(you_lost) 
+
+prompt("FINAL SCORE: You: #{score_count[0]} | Computer: #{score_count[1]}")
+prompt("Thank you for playing. Good bye!")
