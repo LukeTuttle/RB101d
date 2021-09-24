a7c032f4a73765e3c30d96bb749c36125ab27afa
Luke Tuttle <39096324+LukeTuttle@users.noreply.github.com>
2021-09-24 15:01:20 -0600

    moved some files around and added lesson_3 dir

---
+++ lesson_3/pp_easy1.rb
@@ -0,0 +1,21 @@
+# 1. 1 2 2 3 each on a new line 
+
+# 2
+  # 1. does not equal--use for logical test
+  # 2. it means 'is false' -- changes boolean value
+  # 3. it typically means the method mutates the caller but now always
+  # 4. it could be used as a ternary operator
+  # 5. doens't necessarily mean anything -- just part of method name
+  # 6. turn any object into its boolean equivalent
+  
+# 3 advice.gsub!('important, 'urgent')
+  
+# 4 delete_at deletes based on index supplied, delete() removes based on matching value supplied. Both return the last deleted item
+
+# 5 (10..100).cover?(42)
+
+# 6 "Four score and " + famous words ; "Four score and " << famous_words
+
+# 7 flinstones.flatten!
+
+# 8 flinstones.assoc("Barney")
